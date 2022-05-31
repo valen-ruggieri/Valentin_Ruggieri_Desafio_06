4156d36bf83f7055fd195af86b1c8981b266a423
valentin-ruggieri <valeru.251@gmail.com>
2022-05-31 15:21:18 -0300

    Desafio 06 Websocket edit 03

--- public/main.js
+++ public/main.js
@@ -1,4 +1,3 @@
-
 //                    Main.js
 
 // Creamos un constante llamada socket que sea igual a io()
@@ -15,10 +14,11 @@ const divMessages = document.querySelector("#messagesBox");
 const inputAutor = document.querySelector("#autorEmail");
 const inputText = document.querySelector("#message");
 const submit = document.querySelector("#submit");
+const theme = document.querySelector("#theme");
 
 // Una vez realizado la identificacion de estos campos pasamos a darle funcionalidad
 // mediante un addEventListener añadiremos un evento de click a nuestro boton submit
-// este evento tendra la funcion de crear un dateNow que obtiene los valores de la hora, minutos y segundos 
+// este evento tendra la funcion de crear un dateNow que obtiene los valores de la hora, minutos y segundos
 // de cuando se realizo y crearemos el mensaje que es un objeto el cual tiene como props los valores
 // de inputAutor, inputText y dateNow.
 
@@ -26,26 +26,98 @@ const submit = document.querySelector("#submit");
 
 // A continuacion vamos a relizar un socket.emit para emitir el mensaje creado al servidor.
 
+
+
+///////////////////////////////////////////////////////////////////
+const validationAutor = document.getElementById("validationAutor");
+const validationMessage = document.getElementById("validationMessage");
+
+function validationStyle(style) {
+  if (style.value == "white") {
+    return "form-control btn  text-dark bg-light bg-opacity-25";
+  } else {
+    return "form-control btn bg-secondary text-white bg-opacity-10";
+  }
+}
+
+inputAutor.addEventListener("keydown", () => {
+  if (inputAutor.value.length >= 6) {
+    inputAutor.setAttribute(
+      "class",
+      `border border-success border-2 ${validationStyle(theme)}`
+    );
+    validationAutor.textContent = "✅";
+
+    if (inputText.value.length >= 4) {
+      submit.removeAttribute("disabled");
+    }
+  } else if (inputAutor.value.length < 6 && inputAutor.value.length > 0) {
+    inputAutor.setAttribute(
+      "class",
+      `border border-danger border-2 ${validationStyle(theme)}`
+    );
+    validationAutor.textContent = "❌";
+    submit.setAttribute("disabled", "");
+  } else {
+    inputAutor.setAttribute("class", `${validationStyle(theme)}`);
+    validationAutor.textContent = " ";
+    submit.setAttribute("disabled", "");
+  }
+});
+
+inputText.addEventListener("keydown", () => {
+  if (inputText.value.length >= 4) {
+    inputText.setAttribute(
+      "class",
+      `border border-success border-2 ${validationStyle(theme)}`
+    );
+    validationMessage.innerHTML = "✅";
+    if (inputAutor.value.length >= 6) {
+      submit.removeAttribute("disabled");
+    }
+  } else if (inputText.value.length < 4 && inputText.value.length > 0) {
+    inputText.setAttribute(
+      "class",
+      `border border-danger border-2 ${validationStyle(theme)}`
+    );
+    validationMessage.textContent = "❌";
+    submit.setAttribute("disabled", "");
+  } else {
+    inputText.setAttribute("class", `${validationStyle(theme)}`);
+    validationMessage.textContent = " ";
+    submit.setAttribute("disabled", "");
+  }
+});
+
 submit.addEventListener("click", (event) => {
   event.preventDefault();
+  if (inputAutor.value.length >= 6 && inputText.value.length >= 4) {
+    inputText.setAttribute("class", `${validationStyle(theme)}`);
+    inputAutor.setAttribute("class", `${validationStyle(theme)}`);
+    validationMessage.textContent = " ";
+    validationAutor.textContent = " ";
 
-  const date = new Date();
-  const dateNow = ` ${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`;
-  const message = {
-    autor: inputAutor.value,
-    date: dateNow,
-    text: inputText.value,
-  };
-  socket.emit("newMessage", message);
-  inputAutor.value='';
-  inputText.value='';
+    const date = new Date();
+    const dateNow = ` ${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`;
+    const message = {
+      autor: inputAutor.value,
+      date: dateNow,
+      text: inputText.value,
+    };
+    socket.emit("newMessage", message);
+    inputAutor.value = "";
+    inputText.value = "";
+    submit.setAttribute("disabled", "");
+  }
 });
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 // Para pder eliminar nuestros chats vamos a utilizar un boton ya creado en nuestro DOM
 // deleteChatButton es un boton que esta dentro del DOM solo que posee un display: none por el momento
 
-
-const deleteChatButton = document.querySelector('#deleteChat')
+const deleteChatButton = document.querySelector("#deleteChat");
 
 // Realizamos un socket.on para escuchar al servidor y obtener el array 'messages'.
 // Luego de ello colocamos un condicional que nos evalue este array.
@@ -60,16 +132,22 @@ const deleteChatButton = document.querySelector('#deleteChat')
 // Tambien se realiza un socket.emit para emitir los cambios y actualizar del servidor.
 
 socket.on("messages", (messages) => {
-  if(messages.length > 0){
-    deleteChatButton.setAttribute('class',"w-25 text-decoration-none badge bg-danger")
-    divMessages.setAttribute('class',"bg-opacity-10 bg-secondary text-start mb-3 pt-3 w-75 ms-2 ");
+  if (messages.length > 0) {
+    deleteChatButton.setAttribute(
+      "class",
+      "w-25 text-decoration-none badge bg-danger"
+    );
+    divMessages.setAttribute(
+      "class",
+      "bg-opacity-10 bg-secondary text-start mb-3 pt-3 w-75 ms-2 "
+    );
     socket.emit("messages", messages);
-  }else if(messages.length == 0){
-    deleteChatButton.setAttribute('class',"d-none");
-    divMessages.setAttribute('class',"d-none");
+  } else if (messages.length == 0) {
+    deleteChatButton.setAttribute("class", "d-none");
+    divMessages.setAttribute("class", "d-none");
     socket.emit("messages", messages);
-  }})
-
+  }
+});
 
 // Para poder realizar la accion de eliminar el chat realizamos un evento que al presionar el boton
 // realice un socket.emit con la palbra reservada 'deleteChat' el cual realizara un splice del array
@@ -86,14 +164,12 @@ deleteChatButton.addEventListener("click", () => {
 // 3- Crear un condicional que evalue el valor de theme y de acuerdo a este va a cambiar los estilos del mensaje
 // 4- Seleccionar en ambas partes el divMessages que es donde contenermos nuestro elemnto DOM que va a mostar
 // el array de mensajes, y alli relizar un innerHTML el cual cambia su contenido.
-// 5- El contenido del mismo va a ser un mapeo de todo el array messages generando un mensaje el en DOM con todos sus 
+// 5- El contenido del mismo va a ser un mapeo de todo el array messages generando un mensaje el en DOM con todos sus
 // atributos por cada elemento que contenga dicho array.
 // 6- Se le agrega el join('') para pasarlo a formato texto.
 
 // De esta manera va a suceder los mismo en ambos casos, con a unica diferencia de que deendiendo del valor de theme
-// va a cambiar los estilos del mensaje en cuestion. 
-
-const theme = document.querySelector("#theme");
+// va a cambiar los estilos del mensaje en cuestion.
 
 socket.on("messages", (messages) => {
   if (theme.value == "white") {
@@ -122,11 +198,10 @@ socket.on("messages", (messages) => {
   }
 });
 
-
 // Para poder eliminar los productos conntaremos con dos partes:
 
-// 1- Por u lado crear la constante  deleteProducts que nos permite luego 
-// a este boton poder añadirle un evento de click el cual derive en la funcion de 
+// 1- Por u lado crear la constante  deleteProducts que nos permite luego
+// a este boton poder añadirle un evento de click el cual derive en la funcion de
 // emitir mediante socket la la palabra reservada 'deleteProducts'.
 // Esta misma va a ser escuhcada del lado del servidor y por lo tanto realizara un splice dl array 'productos'
 // dejando asi el mismo vacio.
@@ -136,7 +211,6 @@ socket.on("messages", (messages) => {
 // y dentro de la ruta un redireccioamiento hacia la misma, de esta forma actualizando a ruta y reflejando los cambios realizados.
 
 // A modo experimental se podria decir que realizamos una funcion que utiliza Websockets y HTTP
-
 
 const deleteProducts = document.querySelector("#deleteProducts");
 
